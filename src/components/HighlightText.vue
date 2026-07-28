3c17596dbe12072984230b2fb12cec23bc24aa5f
xiangjianan <xiang9872@126.com>
2026-07-28 13:23:18 +0800

    feat: add HighlightText component

---
+++ src/components/HighlightText.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { splitHighlightSegments } from "../utils/searchHighlight";
+
+const props = defineProps<{ text: string; query: string }>();
+
+const normalized = computed(() => props.query.trim().toLowerCase());
+const segments = computed(() => splitHighlightSegments(props.text, normalized.value));
+</script>
+
+<template>
+  <span class="highlight-text">
+    <template v-for="(segment, index) in segments" :key="index">
+      <mark v-if="segment.match">{{ segment.text }}</mark>
+      <template v-else>{{ segment.text }}</template>
+    </template>
+  </span>
+</template>
