446f9d7e08172f9b4a921e2c66cf0c3d04aa3439
xiangjianan <xiang9872@126.com>
2026-09-17 02:00:35 +0800

    release 1.0.175

--- src/state/changelog.ts
+++ src/state/changelog.ts
@@ -29,7 +29,7 @@ export interface ChangelogEntry {
 
 export const CHANGELOG: ChangelogEntry[] = [
   {
-    version: "1.0.174",
+    version: "1.0.175",
     date: "2026-09-17",
     notes: {
       zh: [
